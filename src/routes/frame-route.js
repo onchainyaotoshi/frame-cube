4678a6e0f1d3b521638b22980c1b787d6b6a3475
Yaotoshi <yaotoshinakamoto@gmail.com>
2024-03-04 14:14:19 +0700

    fix website

--- src/routes/frame-route.js
+++ src/routes/frame-route.js
@@ -2,10 +2,11 @@ import express from 'express';
 import RubikRoute from '@routes/rubik-route.js';
 
 import { frameController } from '@controllers/frame-controller.js';
+import { homeController } from '@controllers/home-controller.js';
 
 const router = express.Router();
 
-router.get('/', frameController);
+router.get('/', homeController);
 router.post('/', frameController);
 router.use('/rubik',RubikRoute);
 
